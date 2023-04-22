6ecbaba6b7b395ed5314fc846dd28bd63b468ddc
panisin000 <45750570+panisin000@users.noreply.github.com>
2023-04-22 16:13:38 +0700

    feat add admin guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { RequirementListComponent } from './requirement-list/requirement-list.component';
 import { RequirementFormComponent } from './requirement-form/requirement-form.component';
 import { RequirementApprovalComponent } from './requirement-approval/requirement-approval.component';
+import { adminGuard } from './auth/admin.guard';
 
 // map url => Component
 const routes: Routes = [
   { path: "requirement-list", component: RequirementListComponent },
   { path: "requirement-form", component: RequirementFormComponent },
   { path: "requirement-form/:id", component: RequirementFormComponent },
-  { path: "requirement-approval", component: RequirementApprovalComponent },
+  { path: "requirement-approval", component: RequirementApprovalComponent, canActivate: [adminGuard] },
   {
     path: "auth", loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule),
   }
--- src/app/auth/admin.guard.ts
+++ src/app/auth/admin.guard.ts
@@ -5,6 +5,12 @@ import {
 } from '@angular/router';
 import { AuthService } from './auth.service';
 
+//if want to open requirement-approval => next
+// next ?
+//user.role === 'A' => true => next ได้ไปต่อ requirement-approval
+//user.role != 'A' rounting next แต่กลับ /auth/login
+
+//function same
 export const adminGuard = (next: ActivatedRouteSnapshot) => {
   const loggedInUser = inject(AuthService).getLoggedInUser();
   return loggedInUser?.user.role === 'A'
